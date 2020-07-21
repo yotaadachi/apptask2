2921fdf151416f6f7f56f5b41aed8ecd3aef257b
安達陽太 <yota@MacBook-Pro-3.local>
2020-07-21 14:28:42 +0900

    Finish

--- app/controllers/relationships_controller.rb
+++ app/controllers/relationships_controller.rb
@@ -2,25 +2,23 @@ class RelationshipsController < ApplicationController
 	before_action :set_user, only: [:create, :destroy]
 
   def follows
-  	@user = User.find(params[:id])
-  	@users = @user.follows
-  	render :followers
+    @user = User.find(params[:user_id])
+  	@users = @user.followings
   end
 
   def followers
-  	@user = User.find(params[:id])
+    @user = User.find(params[:user_id])
   	@users = @user.followers
-  	render :followers
   end
 
   def create
   	following = current_user.follow(@user)
   	if following.save
   		flash[:success] = "ユーザーをフォローしました"
-  		redirect_to user_path(@user)
+  		redirect_back(fallback_location: user_path(@user))
   	else
   		flash.now[:alert] = "ユーザーのフォローに失敗しました"
-  		redirect_to user_path(@user)
+      redirect_back(fallback_location: user_path(@user))
   	end
   end
 
@@ -28,10 +26,10 @@ class RelationshipsController < ApplicationController
   	following = current_user.unfollow(@user)
   	if following.destroy
   		flash[:success] = "ユーザーのフォローを解除しました"
-  		redirect_to user_path(@user)
+      redirect_back(fallback_location: user_path(@user))
   	else
   		flash.now[:alert] = "ユーザーのフォロー解除に失敗しました"
-  		redirect_to user_path(@user)
+      redirect_back(fallback_location: user_path(@user))
   	end
   end
 
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,4 @@
 Rails.application.routes.draw do
-  get 'relationships/follows'
-  get 'relationships/followers'
   devise_for :users
   root 'homes#top'
   get 'home/about' => 'homes#about', as:'about'
@@ -8,6 +6,9 @@ Rails.application.routes.draw do
   	resources :book_comments, only: [:create, :destroy]
   	resource :favorites, only: [:create, :destroy]
   end
-  resources :users
+  resources :users do
+  get 'follows' => 'relationships#follows'
+  get 'followers' => 'relationships#followers'
+  end
   resources :relationships, only: [:create, :destroy]
 end
