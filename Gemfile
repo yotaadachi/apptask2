f02d9888e237e0f9c0bf962f600c5d785a03a6b5
安達陽太 <yota@MacBook-Pro-3.local>
2020-07-23 22:37:27 +0900

    Finish

--- Gemfile
+++ Gemfile
@@ -69,3 +69,4 @@ gem 'jquery-rails'
 gem "refile", require: "refile/rails", github: 'manfe/refile'
 gem "refile-mini_magick"
 gem 'font-awesome-sass', '~> 5.4.1'
+gem 'pry-rails'
--- app/controllers/book_comments_controller.rb
+++ app/controllers/book_comments_controller.rb
@@ -1,15 +1,16 @@
 class BookCommentsController < ApplicationController
 	def create
-	  book = Book.find(params[:book_id])
-	  comment = book.book_comments.new(book_comment_params)
+	  @book = Book.find(params[:book_id])
+	  comment = @book.book_comments.new(book_comment_params)
 	  comment.user_id = current_user.id
 	  comment.save
-	  redirect_to book_path(book)
+	  @comment = BookComment.new
 	end
 
 	def destroy
-	  BookComment.find_by(id: params[:id], book_id: params[:book_id]).destroy
-	  redirect_to book_path(params[:book_id])
+	  @comment = BookComment.find_by(id: params[:id], book_id: params[:book_id])
+	  @comment.destroy
+	  @book = @comment.book
 	end
 
 	private
